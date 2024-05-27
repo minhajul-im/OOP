e3b928d59697ce7fc242e1779a96d74e2ed13077
minhajul <dev.minhajul@gmail.com>
2024-05-27 21:23:53 +0600

    sign in page add with chadcn.ui with react-hook-form

---
+++ components/ui/input.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  icon?: React.ReactNode;
+  onShow?: () => void;
+}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, icon, onShow, ...props }, ref) => {
+    return (
+      <div
+        className={cn(
+          "flex h-10 items-center rounded-md border border-input bg-white pl-3 text-sm ring-offset-background focus-within:ring-1 focus-within:ring-ring focus-within:ring-offset-2",
+          className
+        )}>
+        <input
+          type={type}
+          className="w-full p-2 placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+          ref={ref}
+          {...props}
+        />
+        <span onClick={onShow} className="pr-4 cursor-pointer">
+          {icon}
+        </span>
+      </div>
+    );
+  }
+);
+Input.displayName = "Input";
+
+export { Input };
