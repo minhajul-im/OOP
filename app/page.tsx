dccc67e417b0f60b613f09cfd26573a8139aa019
minhajul <dev.minhajul@gmail.com>
2024-11-16 15:17:14 +0600

    oop started

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-const HomePage = () => {
-  return (
-    <main className="container p-4 mx-auto">
-      <h1 className="text-4xl font-bold"> This is Blog Magazine Home Page</h1>
-    </main>
-  );
-};
-
-export default HomePage;
